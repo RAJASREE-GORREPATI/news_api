e82a05011992d0b57670fab36d080ca13e497682
rajasreegorrepati24@gmail.com <rajasreegorrepati24@gmail.com>
2023-09-26 12:15:09 +0530

    send email about title and description from the news

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
 from send_email import send_email
+topic="tesla"
 api_key="f419370ad13e4c408db3bd891c5de31f"
 url="https://newsapi.org/v2/everything?" \
-    "q=tesla&from=2023-08-25&" \
-    "sortBy=publishedAt&apiKey=f419370ad13e4c408db3bd891c5de31f"
+    f"q={topic}&" \
+    "sortBy=publishedAt&" \
+    "apiKey=f419370ad13e4c408db3bd891c5de31f&" \
+    "language=en"
 request=requests.get(url)
 content=request.json()
-body=""
-for article in content["articles"]:
+body = "Subject: Today's news" + "\n"
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body=body+article["title"]+"\n"+article["description"]+2*"\n"
+        body= body + article["title"] + "\n" \
+              + article["description"]\
+              + "\n" + article["url"] + 2*"\n"
 body=body.encode("utf-8")
-send_email(body)
+send_email(message=body)
